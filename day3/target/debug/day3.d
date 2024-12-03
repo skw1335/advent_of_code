9ae34bd9deb8c1fd3968827d3267966b520352cd
skw1335 <samkellmanwanzer@gmail.com>
2024-12-03 17:45:49 -0500

    actually fixed now

--- day3/target/debug/day3.d
+++
@@ -1,1 +0,0 @@
-/home/sam/advent_of_code/day3/target/debug/day3: /home/sam/advent_of_code/day3/src/data.rs /home/sam/advent_of_code/day3/src/main.rs
--- day3/target/debug/deps/day3-8209b407814a1dc3.d
+++
@@ -1,6 +0,0 @@
-/home/sam/advent_of_code/day3/target/debug/deps/day3-8209b407814a1dc3: src/main.rs src/data.rs
-
-/home/sam/advent_of_code/day3/target/debug/deps/day3-8209b407814a1dc3.d: src/main.rs src/data.rs
-
-src/main.rs:
-src/data.rs:
